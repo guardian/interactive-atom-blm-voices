d902c99914e796161ae99986fbaf12391ff75fbb
Harry Fischer <harry.fischer@guardian.co.uk>
2020-07-28 14:51:10 +0100

    added sheet

--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,5 +1,16 @@
-import mainHTML from "./atoms/default/server/templates/main.html!text"
+import mainTemplate from "./atoms/default/server/templates/main.html!text"
 
-export async function render() {
-    return mainHTML;
-} 
+import Mustache from 'mustache'
+import rp from 'request-promise'
+
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/10WlV8bS1zAo9VFARX3bSfQ9y2Rn5onAZMpTPX44XatA.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        console.log(sheets);
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
+}
