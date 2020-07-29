08b91cfd6f254169e7724f2439fa1f832f1a4196
Harry Fischer <harry.fischer@guardian.co.uk>
2020-07-29 18:06:23 +0100

    live version

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -3,8 +3,8 @@ function quoteClicker() {
   var container = document.querySelector('.blm-article-flex');
   console.log(quoteCard);
   console.log(container);
-  for(var i =0; i < quoteCard.length; i++) {
-    quoteCard[i].addEventListener('click', function(){
+  for (var i = 0; i < quoteCard.length; i++) {
+    quoteCard[i].addEventListener('click', function () {
       container.classList.remove('quotes');
       container.classList.add('article');
     });
@@ -13,4 +13,18 @@ function quoteClicker() {
 
 function init() {
   quoteClicker();
-}init();
+}
+init();
+
+
+
+const openQuote = document.getElementsByClassName("mobile-quote")
+console.log(openQuote)
+
+for (var i = 0; i < openQuote.length; i++) {
+  console.log(openQuote[i])
+  openQuote[i].addEventListener("click", function () {
+    console.log(this)
+    this.classList.toggle("clicked")
+  })
+}
